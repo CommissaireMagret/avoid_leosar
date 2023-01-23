88a13898ae174cb65541d3f025c204c8bc87717b
CommissaireMagret <83539452+CommissaireMagret@users.noreply.github.com>
2023-01-23 19:10:28 +0100

    Update gen_lst.sh

--- gen_lst.sh
+++ gen_lst.sh
@@ -6,7 +6,7 @@ pip install pypredict
 
 echo ""
 echo " Utilisation ligne de commande : "
-echo " ./gen_lst.sh <Numéro sequence> <Durée séquence> <Nombre de crénaux> <Nom du fichier lst>"
+echo " ./gen_lst.sh <Numéro sequence> <Durée séquence> <Nombre de crénaux> <Nom du fichier lst> <Date et heure de départ : dd/mm/YYYY-HH:MM:SS>"
 echo ""
 
 if [ -z "$1" ]
@@ -57,6 +57,18 @@ else
         file_lst=$4
 fi
 
+if [ -z "$5" ]
+then
+	read -p "Date et heure de départ (dd/mm/YYYY-HH:MM:SS) :" d_t
+        d_t_lst=$d_t
+        if [ -z "$d_t" ]
+        then
+                d_t_lst=date +"%d/%m/%Y-%T"
+	fi
+else
+        d_t_lst=$5
+fi
+
 #wget https://celestrak.org/NORAD/elements/gp.php?CATNR=38771 -O tle1.txt
 #wget https://celestrak.org/NORAD/elements/gp.php?CATNR=44387 -O tle2.txt
 #wget https://celestrak.org/NORAD/elements/gp.php?CATNR=25338 -O tle3.txt
@@ -65,6 +77,6 @@ fi
 
 #cat tle1.txt tle2.txt tle3.txt tle4.txt tle5.txt > tlesar.txt
 
-python3 lst.py $num $dur $seqs $file_lst
+python3 lst.py $num $dur $seqs $file_lst $d_t_lst
 
 #echo -e "\n$seqs créneaux créés pour la séquence $num de $dur minutes. Liste enregistrée dans "$file_lst"\n"
