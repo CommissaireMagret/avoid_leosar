c5719d9a8ba218229fef8640b9fda0dac52224cd
CommissaireMagret <chemin.rob@gmail.com>
2023-01-25 10:32:47 +0100

    correc_gen_scripts

--- gen_lst.sh
+++ gen_lst.sh
@@ -11,7 +11,7 @@ echo ""
 
 if [ -z "$1" ]
 then
-	read -p "Numéro de la séquence : " n
+	read -p "Numéro de la séquence binaire : " n
 	num=$n
 	if [ -z "$n" ]
 	then
@@ -23,7 +23,7 @@ fi
 
 if [ -z "$2" ]
 then
-	read -p "Durée des séquences en minutes : " d
+	read -p "Durée de la séquence en minutes : " d
 	dur=$d
         if [ -z "$d" ]
         then
@@ -35,7 +35,7 @@ fi
 
 if [ -z "$3" ]
 then
-	read -p "Nombre de créneaux : " s
+	read -p "Nombre de créneaux/répétitions : " s
         seqs=$s
         if [ -z "$s" ]
         then
@@ -59,24 +59,15 @@ fi
 
 if [ -z "$5" ]
 then
-	read -p "Date et heure de départ (dd/mm/YYYY-HH:MM:SS) :" d_t
+	read -p "Date et heure locale de départ (pas UTC) (dd/mm/YYYY-HH:MM:SS) :" d_t
         d_t_lst=$d_t
         if [ -z "$d_t" ]
         then
-                d_t_lst=date +"%d/%m/%Y-%T"
+                d_t_lst=`date +"%d/%m/%Y-%T"`
 	fi
 else
         d_t_lst=$5
 fi
 
-#wget https://celestrak.org/NORAD/elements/gp.php?CATNR=38771 -O tle1.txt
-#wget https://celestrak.org/NORAD/elements/gp.php?CATNR=44387 -O tle2.txt
-#wget https://celestrak.org/NORAD/elements/gp.php?CATNR=25338 -O tle3.txt
-#wget https://celestrak.org/NORAD/elements/gp.php?CATNR=28654 -O tle4.txt
-#wget https://celestrak.org/NORAD/elements/gp.php?CATNR=33591 -O tle5.txt
-
-#cat tle1.txt tle2.txt tle3.txt tle4.txt tle5.txt > tlesar.txt
-
 python3 lst.py $num $dur $seqs $file_lst $d_t_lst
 
-#echo -e "\n$seqs créneaux créés pour la séquence $num de $dur minutes. Liste enregistrée dans "$file_lst"\n"
